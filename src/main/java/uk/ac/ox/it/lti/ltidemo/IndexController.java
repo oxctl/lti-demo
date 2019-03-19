e99bda80a35d62b75af652a03ed0e843459f91cd
Rebecca Miller <rebecca.miller@it.ox.ac.uk>
2019-03-19 15:48:49 +0000

    Add LTI session info to the controller.

--- src/main/java/uk/ac/ox/it/lti/ltidemo/IndexController.java
+++ src/main/java/uk/ac/ox/it/lti/ltidemo/IndexController.java
@@ -1,5 +1,6 @@
 package uk.ac.ox.it.lti.ltidemo;
 
+import edu.ksu.lti.launch.model.LtiSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,7 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class IndexController {
 
     @GetMapping("/")
-    public ModelAndView index() {
+    public ModelAndView index(LtiSession ltiSession) {
         return new ModelAndView("index");
     }
 }
